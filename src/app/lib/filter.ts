2b3d40c6d75176eda8b7730178ca390453cd6d6f
milenagd <milena.delfini@gmail.com>
2025-02-22 17:59:19 +0100

    add apply filters function

---
+++ src/app/lib/filter.ts
@@ -0,0 +1,22 @@
+import { QueryParamsType } from "../page";
+import { EventType } from "../services/fetchEvents";
+
+export const applyFilters = (data: EventType[], filters: QueryParamsType) => {
+    if(!filters.location && !filters.category && !filters.isFree) {
+      return data;
+    }
+    const filteredData = data.filter(i => {
+      let condition = true;
+      if(filters.location) {
+        condition = condition && i.locationName === filters.location
+      }
+      if(filters.category) {
+        condition = condition && i.category === filters.category
+      }
+      if(filters.isFree) {
+        condition = condition && i.isFree.toString() == filters.isFree
+      }
+      return condition;
+    });
+    return filteredData;
+}
